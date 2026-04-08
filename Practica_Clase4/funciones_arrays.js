ee8d5b0b90226548771f6be1b387a09f1204a82e
nahuelemilianoborja33 <nahuelemilianoborja33@gmail.com>
2026-04-08 14:33:27 +0000

    ejercicio 2 agregado

--- Practica_Clase4/funciones_arrays.js
+++ Practica_Clase4/funciones_arrays.js
@@ -8,4 +8,16 @@ function devuelveMensaje(nombre)  {
 
 console.log(devuelveMensaje("Nahuel"))
 
+/* Ejercicio 2) Crear una función que se llame calcularAreaCuadrado que reciba un parámetro que sea el
+lado del cuadrado, calcule el área y la retorne. */
+
+/* Función expresada en forma de declaración, que utiliza un parametro (el lado de un cuadrado al momento de pasarlo como argumento), calcula su area (lado x lado) y devuelve el calculo (tipo number) */
+
+
+function calcularAreaCuadrado (lado) {
+    return lado * lado
+}
+
+console.log(`El area del del cuadrado con lado 50 es: ${calcularAreaCuadrado(50)}`)
+
 
