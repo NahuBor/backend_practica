89c4d2eb417120c4063226521369942f3f3ed622
nahuelemilianoborja33 <nahuelemilianoborja33@gmail.com>
2026-04-08 15:35:51 +0000

    ejercicio 3 agregado

--- Practica_Clase4/funciones_arrays.js
+++ Practica_Clase4/funciones_arrays.js
@@ -20,4 +20,42 @@ function calcularAreaCuadrado (lado) {
 
 console.log(`El area del del cuadrado con lado 50 es: ${calcularAreaCuadrado(50)}`)
 
+/* Ejercicio 3) Crear una función por declaración, puede hacer lo que quieras. */
+
+
+
+function calcularElementosRepetidosString(string, elemento) {
+    // Instancio un array y una variable para utilizar luego
+    let array = []
+    let cantidad = 0;
+    // Hago un for para recorrer el string y agrego cada caracter a el array previamente instanciado */
+    for (let i = 0; i < string.length; i++) {
+        array.push(string[i])
+    }
+    /* Instancio la variable encontrado con el valor retornante de la ejecución del metodo findIndex en el array, 
+    el cual itera los elementos del array, y si se cumple dicha condicion devuelve el indice que la verifica
+    */
+    let encontrado = array.findIndex(x => x === elemento)
+    
+    // Hago un ciclo que se ejecuta bajo la condición que encontrado no sea -1 
+    while (encontrado !== -1) {
+        /* Si encontrado no es igual a -1, entonces por ende hay un indice que tieme como valor que es igual a elemento (objetivo),
+        entonces, cantidad suma 1
+        */
+            cantidad = cantidad + 1
+        /* Utilizo el metodo splice en el array para eliminar del array el indice que cumple la condición
+        Todo esto para que en la sig iteración sea un "nuevo" array, y atrape los nuevos caracteres
+        */
+            array.splice(array.indexOf(elemento),1)
+        /* Para cerrar actualizo la variable encontrado con el valor retornado por el metodo findIndex
+        con el fin de que el while evalue nuvamente la condición y no se quede en un bucle infinito
+        */
+            encontrado = array.findIndex(x => x === elemento)
+        }
+
+    console.log(`La cantidad de caracteres repetidos en el string es: ${cantidad}`)
+
+    }
+
+calcularElementosRepetidosString("oh oh oh","o")
 
