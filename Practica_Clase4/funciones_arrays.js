a8d845e376075d2628606a4a0407c2311114c300
nahuelemilianoborja33 <nahuelemilianoborja33@gmail.com>
2026-04-08 14:20:10 +0000

    ejercicio 1 agregado

--- Practica_Clase4/funciones_arrays.js
+++ Practica_Clase4/funciones_arrays.js
@@ -1,3 +1,11 @@
+/* Ejercicio 1) Crear una función que reciba dos parámetros y retorne un valor. */ 
 
+/* Función que tiene un parametro y devuelve un valor de tipo string */
+
+function devuelveMensaje(nombre)  {
+    return `Hola! Buenos diás, ${nombre}!`
+}
+
+console.log(devuelveMensaje("Nahuel"))
 
 
